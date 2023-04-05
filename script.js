1966baf72a41a964108b2ab28f13a5e94682e84d
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-04-06 03:59:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+describe("Test page", () => {
+  it("Displays main div with heading and paragraph", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".main").should("exist");
+    cy.get("#heading").should("exist");
+  });
+});
